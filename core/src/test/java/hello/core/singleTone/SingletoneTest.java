b0af12f19baf94b09648ae7fbeafd6c3b4b0858c
dlswns2480 <dlswns2480@naver.com>
2023-04-27 23:20:17 +0900

    스프링에서 제공하는 싱글톤 컨테이너 적용

--- core/src/test/java/hello/core/singleTone/SingletoneTest.java
+++ core/src/test/java/hello/core/singleTone/SingletoneTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletoneTest {
 
@@ -36,5 +38,18 @@ public class SingletoneTest {
         System.out.println("singletoneService2 = " + singletoneService2);
 
         Assertions.assertThat(singletoneService1).isSameAs(singletoneService2);
+    }
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println(memberService1);
+        System.out.println(memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
