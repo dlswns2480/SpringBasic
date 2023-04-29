f5f507b99d7f5029cc6198287814ba6c3acca376
dlswns2480 <dlswns2480@naver.com>
2023-04-29 15:28:38 +0900

    filter 를 통해 원하는 annotation만 빈으로 등록

---
+++ core/src/test/java/hello/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,10 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
+//나만의 애노테이션을 만든거임!!!
